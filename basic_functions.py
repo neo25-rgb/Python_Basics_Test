abbd2893e844aee4469a618cbaab6dbd98cd4c9e
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2025-11-03 11:16:57 +0200

    update

--- basic_functions.py
+++ basic_functions.py
@@ -1,22 +1,47 @@
 
 
 def add(num1, num2):
-    pass  # Implement addition
+    sum=num1 + num2
+    return sum
 
 def subtract(num1, num2):
-    pass  # Implement subtraction
+    result=num1 - num2
+    return result
 
 def multiply(num1, num2):
-    pass  # Implement multiplication
+    product=num1 * num2
+    return product
 
 def divide(num1, num2):
-    pass  # Implement division with zero division check
+    # result=num1/num2
+    if num2==0:
+        raise ValueError
+    result=num1/num2
+    return result
+
+    # if num2==0:
+    #     raise ValueError
+    # else:
+        # return result
+
 
 def fizz_buzz(number):
-    pass  # Implement FizzBuzz logic
+    if number%5==0 and number%3==0:
+        return "FizzBuzz"
+    if number%3==0:
+        return "Fizz"
+    if number%5==0:
+        return "Buzz"
+    # if number%5==0 and number%3==0:
+    #     return "FizzBuzz"
+    else:
+        return number
+
 
 def fibonacci(n):
-    pass  # Implement Fibonacci sequence logic
+    n=(n+1)
+    return n
         
 def triangle(n):
+    n=[]
     pass  # Implement triangle generation logic
